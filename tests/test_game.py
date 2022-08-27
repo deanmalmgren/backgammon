6e8e435463ee94d4511bd50c5aaa4cd1f78774dc
Dean Malmgren <dmalmgren@ideo.com>
2022-08-27 10:51:10 -0500

    more game tests

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,8 +1,15 @@
 import unittest
 
+from nose2.tools import params
+
 from backgammon import Game
 
 class TestGame(unittest.TestCase):
     def test_game_value(self):
         game = Game()
         self.assertEqual(game.game_value, 1)
+
+    @params(1, 'a')
+    def test_not_player(self, player1):
+        with self.assertRaises(TypeError):
+            game = Game(player1)
